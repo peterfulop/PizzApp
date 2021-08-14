2b0362b1ceb581e212f539281d93d80e1232c2b7
Peter <mrpeterfulop@gmail.com>
2021-08-15 00:59:18 +0200

    08.15. hidratation updated

--- JS/config.js
+++ JS/config.js
@@ -1,6 +1,12 @@
 export const doughRatio = {
-    flour: 0.616666667,
+    flour: 0.59,
     salt: 0.016666667,
-    water: 0.366666667,
+    water: 0.34,
     yeast: 7
 };
+// export const doughRatio = {
+//     flour: 0.616666667,
+//     salt: 0.016666667,
+//     water: 0.366666667,
+//     yeast: 7
+// };
